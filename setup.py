beb26227844ffc1d85f169839270fa98d3339b15
Maloji <malojighorpade07@gmail.com>
2026-02-20 23:38:45 +0530

    Project structure is rady using cookiecutler

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='i building capstone project',
+    author='MG_services',
+    license='',
+)
