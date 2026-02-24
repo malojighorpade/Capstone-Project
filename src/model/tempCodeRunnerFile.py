cdff1ad96ce4520b09dd0234deacffa211446faa
Maloji <malojighorpade07@gmail.com>
2026-02-24 14:58:04 +0530

    done till model registrstion

---
+++ src/model/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+dagshub_token = os.getenv("CAPSTONE_TEST")
+# if not dagshub_token:
+#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+
+# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+# dagshub_url = "https://dagshub.com"
+# repo_owner = "vikashdas770"
+# repo_name = "YT-Capstone-Project"
